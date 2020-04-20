fcbcf15f79570731090f93254aca8c05ebc8c7f7
Andres Donoso <af.donoso@uniandes.edu.co>
2020-04-20 09:45:17 -0500

    Retos terminados

--- src/components/job.js
+++ src/components/job.js
@@ -1,17 +1,46 @@
 import React from 'react';
+import { FormattedDate, FormattedNumber, FormattedPlural } from 'react-intl'
 
 export default class Job extends React.Component {
 
-  	render() {
-  		return (
-  			<tr>
-  				<th scope="row">{this.props.offer.id}</th>
-      			<td>{this.props.offer.name}</td>
-  				<td>{this.props.offer.company}</td>
-      			<td>{this.props.offer.salary}</td>
-      			<td>{this.props.offer.city}</td>
-      			<td>{this.props.offer.date}</td>
-  			</tr>
-  		);
+	render() {
+		var formattedPlural;
+		if (navigator.language.startsWith("en")) {
+			formattedPlural = <FormattedPlural
+				value={this.props.offer.salary}
+				one="million"
+				other="millions"
+			/>
+		} else {
+			formattedPlural = <FormattedPlural
+				value={this.props.offer.salary}
+				one="millón"
+				other="millones"
+			/>
+		}
+
+		return (
+			<tr>
+				<th scope="row">{this.props.offer.id}</th>
+				<td>{this.props.offer.name}</td>
+				<td>{this.props.offer.company}</td>
+				<td>
+					{this.props.offer.salary} {formattedPlural}
+				</td>
+				<td>{this.props.offer.city}</td>
+				<td>
+					<FormattedDate
+						value={this.props.offer.date}
+						year='numeric'
+						month='long'
+						day='numeric'
+						weekday='long'
+					/>
+				</td>
+				<td>
+					<FormattedNumber value={this.props.offer.visits} />
+				</td>
+			</tr>
+		);
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from "react-intl";
 
 import JobsList from "./components/jobsList";
+import localEsMessages from './locales/es.json'
+import localEnMessages from './locales/en.json'
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+let translationTable = navigator.language.startsWith("es") ? localEsMessages : localEnMessages
+console.log(navigator.language)
+
+ReactDOM.render(
+    <IntlProvider locale={navigator.language} messages={translationTable}>
+        <JobsList />
+    </IntlProvider>, document.getElementById("root")
+);
